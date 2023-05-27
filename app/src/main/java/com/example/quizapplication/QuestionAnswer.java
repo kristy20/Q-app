373e9137e2a92f6b391b9cb1ef0d421bd586a3bc
krist <kristyonut@gmail.com>
2023-05-27 15:21:46 +0300

    upload

---
+++ app/src/main/java/com/example/quizapplication/QuestionAnswer.java
@@ -0,0 +1,50 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+    public static String questions[]={
+            "What memory below is the largest?",
+            "What do you use to click stuff?",
+            "What is the most used search engine?",
+            "What operation system dose people use the most?",
+            "What do you call a portable computer?",
+            "Which line do you use to connect to the internet?",
+            "What do you use to type?",
+            "What do you use to read CDs/DVDs?",
+            "What chat program does people around the world use most?",
+            "What do you do if your computer dies/malfunctions?"
+
+
+    };
+    public static String choices[][]={
+            {"1TB","1GB","10GB","55000Bytes"},
+            {"RAM","CPU","Mouse","Keyboard"},
+            {"Yahoo","Google","Bing","SOSO"},
+            {"Windows 95","Windows 10","Linux","MAC OX 10 Snow Leopard"},
+            {"Laptop","Bookshop","Computer","CPU"},
+            {"Motherboard","Mouse","Ethernet","DBM"},
+            {"Keyboard","Mouse","ROM","Arrow"},
+            {"DVD ROM","CD DVD PLAYER","CD ROM","CD DVD ROM"},
+            {"MSN Messenger","Yahoo! Messenger","Skype","ICall"},
+            {"Reboot(restart)","Shut Down","Call a person to fix it","Recovery CD and Recover your system"}
+
+
+
+
+
+    };
+
+    public static String correctAnswer[]={
+            "1TB",
+            "Mouse",
+            "Google",
+            "Windows 10",
+            "Laptop",
+            "Ethernet",
+            "Keyboard",
+            "CD DVD PLAYER",
+            "MSN Messenger",
+            "Recovery CD and Recover your system"
+    };
+
+
+}
